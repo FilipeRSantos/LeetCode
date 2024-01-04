14b666822746fba6629d1ff6c7662958ed6ab71a
Filipe Santos <fprssantos@gmail.com>
2024-01-04 10:22:30 -0300

    Refactor and MergeSortedArray solution

---
+++ Solutions/MergeSortedArray.cs
@@ -0,0 +1,20 @@
+﻿namespace Solutions;
+
+public static class MergeSortedArray {    
+    public static void Run(int[] nums1, int m, int[] nums2, int n) {
+        var index1 =  m - 1;
+        var index2 = n - 1;
+        for (var i = m + n - 1; i >= 0; i--) {
+            var num2Greater = 
+                index2 >= 0 && (index1 < 0 || nums2[index2] > nums1[index1]);
+
+            if (num2Greater) {
+                nums1[i] = nums2[index2];
+                index2 -= 1;
+            } else {
+                nums1[i] = nums1[index1];
+                index1 -= 1;
+            }
+        }
+    }
+}
---
+++ Tests/MergeSortedArrayTests.cs
@@ -0,0 +1,18 @@
+﻿using Solutions;
+
+namespace Tests;
+
+public class MergeSortedArrayTests
+{
+    [Theory]
+    [InlineData(new []{ 1,2,3,0,0,0 }, 3, new []{ 2,5,6 }, 3, new []{ 1,2,2,3,5,6 } )]
+    [InlineData(new []{ 1 }, 1, new int[]{ }, 0, new []{ 1 } )]
+    [InlineData(new []{ 0 }, 0, new []{ 1 }, 1, new []{ 1 } )]
+    
+
+    public void Test1(int[] nums1, int m, int[] nums2, int n, int[] expected)
+    {
+        MergeSortedArray.Run(nums1, m, nums2, n);
+        Assert.Equal(nums1, expected);
+    }
+}
