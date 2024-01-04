f42f34650b5714d0cabfb6cf6aa3de724906cee1
Filipe Santos <fprssantos@gmail.com>
2024-01-04 10:23:00 -0300

    Refactor and MergeSortedArray solution (#1)

---
+++ Solutions/TwoSum.cs
@@ -0,0 +1,20 @@
+﻿namespace Solutions;
+
+public static class TwoSum {    
+    public static int[] Run(int[] nums, int target) {
+        var knownValues = new Dictionary<int, int>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var expected = target - nums[i];
+            if (knownValues.TryGetValue(expected, out var value))
+            {
+                return new[] { value, i };
+            }
+            
+            knownValues.Add(nums[i], i);
+        }
+
+        throw new Exception("Not supposed to happen");
+    }
+}
---
+++ Tests/TwoSumTests.cs
@@ -0,0 +1,17 @@
+﻿using Solutions;
+
+namespace Tests;
+
+public class TwoSumTests
+{
+    [Theory]
+    [InlineData(new []{ 2,7,11,15 }, 9, new []{ 0,1 })]
+    [InlineData(new []{ 3,2,4 }, 6, new []{ 1,2 })]
+    [InlineData(new []{ 3,3 }, 6, new []{ 0,1 })]
+    [InlineData(new []{ -3,4,3,90 }, 0, new []{ 0,2 })]
+    public void Test1(int[] nums, int target, int[] expected)
+    {
+        var received = TwoSum.Run(nums, target);
+        Assert.Equal(received.Order(), expected.Order());
+    }
+}
