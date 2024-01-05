d39c97b28784813a3a14e01daee0265b81277f87
Filipe Santos <fprssantos@gmail.com>
2024-01-05 10:45:33 -0300

    feat: completed candy solution

---
+++ Solutions/Candy.cs
@@ -0,0 +1,35 @@
+﻿namespace Solutions;
+
+//https://leetcode.com/problems/candy/
+public static class Candy {    
+    public static int Run(int[] ratings)
+    {
+        var lastAmmount = 1;
+        var candies = new int [ratings.Length];
+        
+        for (var i = 0; i < ratings.Length; i++)
+        {
+            var greatherThanLeft = i > 0 ? ratings[i] > ratings[i - 1] : false;
+
+            if (greatherThanLeft)
+                lastAmmount++;
+            else
+                lastAmmount = 1;
+
+            candies[i] = lastAmmount;
+        }
+        
+        for (var i = ratings.Length - 1; i >= 0; i--)
+        {
+            var greatherThanRight = i < ratings.Length - 1 ? ratings[i] > ratings[i + 1] : false;
+
+            if (!greatherThanRight) 
+                continue;
+            
+            if (candies[i] <= candies[i + 1])
+                candies[i] = candies[i + 1] + 1;
+        }
+        
+        return candies.Sum();
+    }
+}
---
+++ Tests/CandyTests.cs
@@ -0,0 +1,18 @@
+﻿using Solutions;
+
+namespace Tests;
+
+public class CandyTests
+{
+    [Theory]
+    [InlineData(new []{ 1,0,2 }, 5)]
+    [InlineData(new []{ 1,2,2 }, 4)]
+    [InlineData(new []{ 1,3,3,4 }, 6)]
+    [InlineData(new []{ 29,51,87,87,72,12 }, 12)]
+    [InlineData(new []{ 1,3,4,5,2 }, 11)]
+    public void Test1(int[] ratings, int expected)
+    {
+        var output = Candy.Run(ratings);
+        Assert.Equal(expected, output);
+    }
+}
